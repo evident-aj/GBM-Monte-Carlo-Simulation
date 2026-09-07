79e10f7572244ac460d2eb36eb9078687c28b650
Aidan Hugo <aidanhugo@Aidans-MacBook-Pro.local>
2026-09-07 16:25:31 -0400

    Fully working GBM simulation

--- montecarlosim.py
+++ montecarlosim.py
@@ -9,7 +9,9 @@ while True:
     user_input = input("What stock would you like to analyze (e.g., AAPL): ")
     ticker_symbol = user_input.strip().upper()
     stock = yf.Ticker(ticker_symbol)
-    past = stock.history(period="252d")['Close']
+    info = stock.info
+    name = info.get("longName", "N/A")
+    past = stock.history(period="750d")['Close']
     if past.empty:
         print("No price data found for this ticker symbol.")
         continue
@@ -21,7 +23,7 @@ log_returns = np.log(1 + pct_change_res)
 
 mean_daily_return = log_returns.mean()
 daily_vol = log_returns.std()
-dt = 1/252
+dt = 1/750
 current_price = past.iloc[-1]
 
 
@@ -40,16 +42,16 @@ for i in range(1000):
     all_paths.append(path)
     all_prices.append(path[-1])
 
-cmp = plt.get_cmap('RdYlBu')
+cmp = plt.get_cmap('plasma')
 color_norm = clr.Normalize(min(all_prices),max(all_prices))
 
 plt.subplot(1, 2, 1)
 for path in all_paths:
     color = cmp(color_norm(path[-1]))
     plt.plot(path, color=color, alpha=0.3)
-plt.title(f"GBM Monte Carlo Simulation\nTicker: {ticker_symbol}")
 plt.xlabel("Days")
 plt.ylabel("Stock Price $")
+
 
 plt.subplot(1, 2, 2)
 counts, bin_edges, bars = plt.hist(all_prices, orientation='horizontal')
@@ -57,6 +59,11 @@ for i in range(len(bars)):
     midpoint = (bin_edges[i] + bin_edges[i + 1])/2
     bars[i].set_facecolor(cmp(color_norm(midpoint)))
 plt.xlabel("Frequency") 
+
+VaR = round(np.percentile(all_prices, 5), 2)
+
+plt.suptitle(f"GBM Monte Carlo Simulation\nStock: {name}\nVaR: ${VaR}")
+
 
 plt.tight_layout()
 plt.show()
